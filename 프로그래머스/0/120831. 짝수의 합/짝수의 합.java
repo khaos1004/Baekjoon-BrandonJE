883bffc8f344b82ce2b6bc9ff65c3d9c804f842e
Brandon JE <78074606+khaos1004@users.noreply.github.com>
2024-04-05 13:12:25 +0900

    [level 0] Title: 짝수의 합, Time: 1.45 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.java
@@ -0,0 +1,11 @@
+import java.util.stream.IntStream;
+
+class Solution {
+    public int solution(int n) {
+        
+        return IntStream.range(1, n + 1)
+                        .filter(i -> i % 2 == 0)
+                        .sum();
+
+    }
+}
