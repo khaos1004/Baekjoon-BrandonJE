b1c0a789c00253eb68f87c7dcca51124e9f09f71
Brandon JE <78074606+khaos1004@users.noreply.github.com>
2024-04-15 13:40:17 +0900

    [level 0] Title: 짝수는 싫어요, Time: 2.81 ms, Memory: 74 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,11 @@
+import java.util.stream.IntStream;
+
+class Solution {
+    public int[] solution(int n) {
+        int[] answer = {};
+       answer = IntStream.range(0, n + 1 )
+                 .filter(i -> i % 2 != 0 )
+                 .toArray();
+        return answer;
+    }
+}
